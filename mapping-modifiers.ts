daa48e469504a9d50455e68aa81f6a7637f34a88
Kashif Ali <jaxkashif34@gmail.com>
2023-05-09 18:01:41 +0500

    complete mapping

--- mapping-modifiers.ts
+++ mapping-modifiers.ts
@@ -51,4 +51,68 @@
   };
 
   type GetUser = MapTypeWithNewPropertiesAndSubProperties<MapUser>;
+
+  // Remove the property from Object Type
+
+  type RemoveProperty<Object_Type> = {
+    [property in keyof Object_Type as Omit<property, 'kind'> & string]: Object_Type[property];
+  };
+
+  interface Circle {
+    kind: 'circle';
+    radius: number;
+  }
+
+  type KindLessCircle = RemoveProperty<Circle>; // In this type we remove the kind property from Circle type
+
+  //   const circle: Circle = {
+  //     kind: 'circle',
+  //     radius: 10,
+  //   };
+
+  type EventConfig<Events extends { kind: string }> = {
+    // Question: what above line of code is doing ?
+    // Answer: it is creating a type which have a property of kind and the value of kind is string
+    // Question: it's ensuring that whatever we provide to EventConfig it must have kind property ?
+    // Answer: Yes
+    [E in Events as E['kind']]: (event: E) => void;
+    // Question: what above line of code is doing ?
+    // Answer: it is creating a type which have a property of kind and the value of kind is string and the value of kind is a function which take a parameter of type E and return void
+    // Question: what is E in above line of code ?
+    // Answer: it is a generic type which is extending the Events type and Events type is a type which have a property of kind and the value of kind is string
+    // Question: what is E['kind'] in above line of code ?
+    // Answer: it is a type which is a string
+    // Question: why we use as E['kind'] ?
+    // Answer: because we want to use the value of kind property as a key of the object
+    // Question: is E iterates over each property of Events and assign each property a name of whatever comes from kind value. Am I right correct me if i'm wrong ?
+    // Answer: Yes
+    // Question: when E iterates over Event the possible value of E might beSquareEvent or CircleEvent. Am I right ?
+    // Answer: Yes
+    // Question: when E iterates over Event what might be the possible value of E ?
+    // Answer: it might be SquareEvent or CircleEvent
+  };
+
+  type SquareEvent = { kind: 'square'; x: number; y: number };
+  type CircleEvent = { kind: 'circle'; radius: number };
+
+  type Config = EventConfig<SquareEvent | CircleEvent>; // In this type we have a object which have two functions which accepts a parameter of type SquareEvent and CircleEvent and return void respectively
+  const config: Config = {
+    circle: (e) => e.radius,
+    square: (e) => e.x,
+  };
+
+  //   Mapped type with Conditional type
+
+  type ExtractPII<Object_Type> = {
+    [property in keyof Object_Type]: Object_Type[property] extends { pii: true } ? true : false;
+  };
+
+  //   extends { pii: true } this is used to ensure that if the provided Object_Type have that property with same data-type
+
+  type DBFields = {
+    id: { format: 'incrementing' };
+    name: { type: string; pii: true };
+  };
+
+  type ObjectNeedingGDPRDeletion = ExtractPII<DBFields>;
 })();
