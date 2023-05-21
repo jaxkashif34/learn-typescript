e619c24d08dfcfe5cef68ed31132d7f709eb25d9
Kashif Ali <jaxkashif34@gmail.com>
2023-05-22 00:31:13 +0500

    trying to complete type-challenges

---
+++ type-challanges/Captilized.ts
@@ -0,0 +1,3 @@
+type MyCapitalize<S extends string> = S extends `${infer FirstLetter}${infer Rest}` ? `${Uppercase<FirstLetter>}${Rest}` : S;
+
+type A45 = MyCapitalize<'hello world'>; // expected to be 'Hello world'
