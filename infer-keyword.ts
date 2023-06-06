028c98a6f9ba1cf1a7590cd37d294a7fb2bd2064
Kashif Ali <jaxkashif34@gmail.com>
2023-06-06 14:00:55 +0500

    i think that's enough for now

--- infer-keyword.ts
+++ infer-keyword.ts
@@ -4,7 +4,7 @@
   type Result1 = boolean extends true ? 'yes' : 'no'; // '
 
   const func = (check: boolean) => {
-    return "1234"
+    return '1234';
   };
 
   type TypeOfFunc = typeof func; // (check: boolean) => number
@@ -27,29 +27,27 @@
 
   type ResultType4 = OnlyStringReturnTypeCleaner<typeof func>;
 
-  type OnlyStringReturnTypeMoreNest<T> = 
-    T extends (...args: any[]) => infer R ?
-        R extends Promise<infer U> ?
-            U extends Array<infer S> ?
-                S extends string ?
-                    S : 
-                never : 
-            never : 
-        never : 
-    never;
+  type OnlyStringReturnTypeMoreNest<T> = T extends (...args: any[]) => infer R ? (R extends Promise<infer U> ? (U extends Array<infer S> ? (S extends string ? S : never) : never) : never) : never;
 
-  type ResultType5 = OnlyStringReturnTypeMoreNest<()=>Promise<Array<"1"|"2">>>;
+  type ResultType5 = OnlyStringReturnTypeMoreNest<() => Promise<Array<'1' | '2'>>>;
 
+  type Multiplier<T> = T extends `${infer U extends number}x` ? U : never;
 
-  type Multiplier<T> = T extends `${infer U extends number}x` ? U :never
-  
-  type M = Multiplier<"2.0x" | "3x" | "4x"> // if we pass "2.0x" this will only return number
+  type M = Multiplier<'2.0x' | '3x' | '4x'>; // if we pass "2.0x" this will only return number
 
+  type NYAreaCode = 934 | 680 | 332 | 838;
 
-  type NYAreaCode = 934 | 680 | 332 | 838
+  type CodeValidator<T> = T extends `${infer P1 extends NYAreaCode}-${infer P2 extends number}-${infer P3 extends number}` ? true : false;
 
-  type CodeValidator<T> = 
-    T extends `${infer P1 extends NYAreaCode}-${infer P2 extends number}-${infer P3 extends number}`? true : false 
-    
-    type Phone = CodeValidator<"123-123-123">
+  type Phone = CodeValidator<'123-123-123'>;
+
+  type InferSomething<T> = T extends infer U ? U : never;
+  type Inferred = InferSomething<"I'm a string">;
+  // Question: what will be the value of U in the above example ?
+  // Answer: U will be "I'm a string" infer is a keyword that is used to infer (andaza legana) the type of a generic type
+
+  // example 2
+
+  type InferSomething2<T> = T extends { a: infer U; b: number } ? U : never;
+  type Inferred2 = InferSomething2<{ a: 'Hello'; b: 2 }>; // Inferred2 will be "Hello"
 })();
